616ca368b1554e278667437c3e82bc1209169442
Dither <Dither@users.noreply.github.com>
2016-12-11 15:52:47 +0300

    Fix for Firefox 49+

--- bootstrap.js
+++ bootstrap.js
@@ -8,49 +8,67 @@ const Ci = Components.interfaces;
 
 Components.utils.import("resource://gre/modules/Services.jsm");
 
-function jpToggleF12ForWindow(window, disabledValue) {
-	let F12key = window.document.getElementById("key_devToolboxMenuItemF12");
-	F12key.setAttribute("disabled", disabledValue);
+function toggleF12ForWindow(aWindow, disabledValue) {
+	let F12key = aWindow.document.getElementById("key_devToolboxMenuItemF12");
+	if (F12key) {
+		F12key.setAttribute("disabled", disabledValue);
+		return;
+	}
+
+	let tryDelay = 5, tryNum = 0, tryMax = 100000;
+	let timer = Cc["@mozilla.org/timer;1"].createInstance(Ci.nsITimer);
+
+	let delayCheck = function() {
+		tryNum += tryDelay;
+		if (tryNum >= tryMax) {
+			timer = null;
+			return;
+		}
+		timer.init(timerObserver, tryDelay, timer.TYPE_ONE_SHOT);
+		if (tryDelay < 500 && (tryDelay *= 2) > 500) tryDelay = 500;
+	};
+
+	let timerObserver = {
+		observe: function() {
+			timer.cancel();
+			F12key = aWindow.document.getElementById("key_devToolboxMenuItemF12");
+			if (F12key) {
+				F12key.setAttribute("disabled", disabledValue);
+				timer = null;
+			} else delayCheck();
+		}
+	};
+
+	delayCheck();
 }
 
-var jpF12WindowListener = {
+function installF12Listener(start) {
+	let wm = Services.wm,
+		windows = wm.getEnumerator("navigator:browser");
+	while (windows.hasMoreElements()) toggleF12ForWindow(windows.getNext(), start);
+	wm[start ? "addListener" : "removeListener"](F12WindowListener);
+}
+
+var F12WindowListener = {
 	onOpenWindow: function(aWindow) {
-	
-		let domWindow = aWindow.QueryInterface(Ci.nsIInterfaceRequestor).
-			getInterface(Ci.nsIDOMWindowInternal || Ci.nsIDOMWindow);
-  
-	  domWindow.addEventListener("load", function onLoad() {
-      domWindow.removeEventListener("load", onLoad, false);
-      jpToggleF12ForWindow(domWindow, true);	
-    }, false);	
+		let domWindow = aWindow.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIDOMWindow);
+		domWindow.addEventListener("load", function onLoad() {
+			domWindow.removeEventListener("load", onLoad, false);
+			if (domWindow.document.documentElement.getAttribute("windowtype") !== "navigator:browser") return;
+			toggleF12ForWindow(domWindow, true);
+		}, false);
 	},
- 
 	onCloseWindow: function(aWindow) {},
 	onWindowTitleChange: function(aWindow, aTitle) {}
 };
 
 function startup(data, reason) {
-	let wm = Services.wm;
-	let windows = wm.getEnumerator("navigator:browser");
-	
-	while (windows.hasMoreElements()) {
-		let window = windows.getNext();
-		jpToggleF12ForWindow(window, true);
-	}
-	
-	wm.addListener(jpF12WindowListener);
+	installF12Listener(true);
 }
 
 function shutdown(data, reason) {
-	let wm = Services.wm;
-	let windows = wm.getEnumerator("navigator:browser");
-	
-	while (windows.hasMoreElements()) {
-		let window = windows.getNext();
-		jpToggleF12ForWindow(window, false);
-	}
-	
-	wm.removeListener(jpF12WindowListener);
+	if (reason === APP_SHUTDOWN) return;
+	installF12Listener(false);
 }
 
 function install(data, reason) {}
